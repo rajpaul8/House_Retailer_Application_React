c0b3e01d0c3ce5bb84569ce619a1eeee847361e4
rajpaul8 <rajpaul83@gmail.com>
2022-07-14 09:57:45 +0530

    cleaned up code and Added Firebase Config fil & Stylesheet

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCglVIfInAvVp14ykIo2yxBZHyZ8sARtk4",
+  authDomain: "house-retailer-app.firebaseapp.com",
+  projectId: "house-retailer-app",
+  storageBucket: "house-retailer-app.appspot.com",
+  messagingSenderId: "341451477495",
+  appId: "1:341451477495:web:3c398540df44fd3ba95ef2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
