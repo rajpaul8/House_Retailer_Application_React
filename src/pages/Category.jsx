4f3baa9a3ef3db38c511b343011f2daf2929cd0e
rajpaul8 <rajpaul83@gmail.com>
2022-07-15 21:49:17 +0530

    Fetch Listings from Firebase

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,6 +1,83 @@
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Category() {
-  return <div>Category</div>;
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const params = useParams();
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        // Get a reference
+        const listingsRef = collection(db, "listings");
+
+        // create a query
+        const q = query(
+          listingsRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        //   Execute the query
+        const querySnap = await getDocs(q);
+
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        console.log(listings);
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fetch the data");
+      }
+    };
+    fetchListing();
+  }, []);
+  return (
+    <>
+      <div className="category">
+        <header>
+          <p className="pageHeader">
+            {params.categoryName === "rent"
+              ? "Places for rent"
+              : "Places for sale"}
+          </p>
+        </header>
+        {loading ? (
+          <Spinner></Spinner>
+        ) : listings && listings.length > 0 ? (
+          <>
+            <main>
+              <ul className="categoryListings">
+                {listings.map((listing) => {
+                  return <h3>{listing.data.name}</h3>;
+                })}
+              </ul>
+            </main>
+          </>
+        ) : (
+          <p>No Listings for {params.categoryName}</p>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Category;
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -20,10 +20,10 @@ function Explore() {
             />
             <p className="exploreCategoryName">Places for rent</p>
           </Link>
-          <Link to="/category/sell">
+          <Link to="/category/sale">
             <img
               src={sellCategoryImage}
-              alt="Sell"
+              alt="Sale"
               className="exploreCategoryImg"
             />
             <p className="exploreCategoryName">Places for sale</p>
